3044aa6a1923e71ce28b1ab36a19c06ab365b3cf
arthp <agrearth22@gmail.com>
2025-06-24 15:49:35 +0530

    Database name renamed

--- Dalc/settings.py
+++ Dalc/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'Dalc.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'CRUDDB.db',
+        'NAME': BASE_DIR / 'ParaDox.db',
     }
 }
 
